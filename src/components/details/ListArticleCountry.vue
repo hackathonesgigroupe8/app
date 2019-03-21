3822176969d6ae435f73c2b24d26a4dbb93eb346
Dylan Settbon <dylans_set@hotmail.fr>
2019-03-21 18:42:30 +0100

    [ADD] Article list

--- src/components/details/ListArticleCountry.vue
+++ src/components/details/ListArticleCountry.vue
@@ -1,44 +1,108 @@
 <template>
     <div>
-        <a href="#" v-on:click="showDetails" class="closebtn" id="closeNav"></a>
-        <h1 style="margin-bottom: 32px;">All articles for France </h1>
+        <a href="#" v-on:click="showDetails" class="closebtn" id="closeNavbar"><</a>
+        <h1 style="margin-bottom: 32px;">All articles for {{ country.name }} </h1>
 
 
         <div style="margin-bottom: 10px;">
-            <a href="#" class="hover">Biodiversity</a>
-            <a href="#" class="hover">Biodiversity</a>
-            <a href="#" class="hover">Biodiversity</a>
-            <a href="#" class="hover">Biodiversity</a>
+            <div class="hover" v-for="(article,index) in nbArticles" :key="article.id">
+                <a href="#" v-on:click="showCategory(index)" >
+                    {{ article['categ'].charAt(0).toUpperCase() + article['categ'].slice(1) }}
+                </a>
+                <div class="hover2 display" v-for="article in article.list.slice(0, 1)" :key="article.id" :id='index'>
+                    <a target="_blank" :href="article.url">{{ article.name }}</a>
+                </div>
+            </div>
         </div>
-
-
-        <div>
-            <a href="#" class="hover2">The biodiverity in Paris since 2018rsity</a>
-            <a href="#" class="hover2">The biodiverity in Paris since 2018</a>
-            <a href="#" class="hover2">The biodiverity in Paris since 2018rsity</a>
-            <a href="#" class="hover2">The biodiverity in Paris since 2018</a>
-            <a href="#" class="hover2">The biodiverity in Paris since 2018rsity</a>
-            <a href="#" class="hover2">The biodiverity in Paris since 2018</a>
-            <a href="#" class="hover2">The biodiverity in Paris since 2018rsity</a>
-            <a href="#" class="hover2">The biodiverity in Paris since 2018</a>
-        </div>
-
-        <button class="share">Share result</button>
-
     </div>
-    
+
 </template>
 
 
 <script>
+    import axios from 'axios';
+
     export default {
-        name: 'ListArticleCountry',props: ['showDetails'],
+        name: 'ListArticleCountry',
+        props: {
+            country: Object,
+            showDetails: Function,
+        },
+        data() {
+            return ({
+                nbArticles: [],
+
+            })
+        },
+        methods: {
+            getData(code, dataset) {
+                let vm = this;
+                const requestBody = {
+                    "code": code,
+                    "dataset": dataset,
+                };
+
+                const config = {
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                };
+                axios.post('http://localhost:8060/stats', requestBody, config)
+                    .then((res) => {
+                        console.log(res);
+                        vm.nbArticles = res.data.articles;
+
+                    })
+                    .catch((err) => {
+                        return err;
+                    })
+            },
+            showCategory(id){
+                var divArticles = document.getElementsByClassName("display");
+                Array.from(divArticles).forEach((el) =>{
+                    el.style.display = "none"
+                })
+                document.getElementById(id).style.display = "block"
+            }
+        },
+        mounted() {
+            this.getData('FR', 'eea_ip_2018');
+        }
     }
+
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
+    .display{
+        display: none;
+    }
+    .hover {
+        padding: 5px 14px 14px 0px !important;
+        text-decoration: none !important;
+        font-size: 18px !important;
+        font-weight: 500;
+        font-style: normal;
+        font-stretch: normal;
+        display: inline-block !important;
+        color: #9ba0a5 !important;
 
+    }
 
+    .hover2 a {
+        padding: 5px 14px 14px 0px !important;
+        text-decoration: underline !important;
+        font-size: 14px !important;
+        font-weight: 500;
+        font-style: normal;
+        font-stretch: normal;
+        color: #4a5056 !important;
+        cursor: pointer;
+    }
+    
+    .article{
+        display: none;
+    }
 </style>
 
