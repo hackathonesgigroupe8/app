06c5517979dc4cf53526993ebcd62690a63d2463
Wladimir Delenclos <wdelenclos@users.noreply.github.com>
2019-03-22 11:56:17 +0100

    [Archive] Auto merge

--- src/components/Services.vue
+++ src/components/Services.vue
@@ -4,7 +4,7 @@
             <h1>Our services</h1>
             <h3>A partner for your strategic intelligence</h3>
             <p>EAR is a platform that mesure hot topics and trend subjects.<br>Our know-how provides you vigil and analytics about your public opinion perception </p>
-            <p>Our company can also help you to know how to use the business platform. Don't hesitate to contact our <services class=""></services></p>
+            <p>Our company can also help you to know how to use the business platform. Don't hesitate to contact our services.</p>
             <a href="mailto:services-client@ecoaware.com" class="styledBtn">Contact us</a>
         </div>
         <div class="infoPageImg serviceImg"></div>
